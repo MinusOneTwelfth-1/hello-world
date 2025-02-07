62132a485457ba005979f7df8d72f346fdaf9169
MinusOneTwelfth-1 <56498515+MinusOneTwelfth-1@users.noreply.github.com>
2025-02-07 14:36:07 +0530

    Update worklets.js

--- docs/audioWorklet/worklets.js
+++ docs/audioWorklet/worklets.js
@@ -1,3 +1,4 @@
+// sine wave
 registerProcessor('noise-generator',class extends AudioWorkletProcessor {
   process(inputs, outputs) {
     for (let i=0;i<outputs[0][0].length;++i)  outputs[0][0][i]=2*Math.Sin(i/100)
