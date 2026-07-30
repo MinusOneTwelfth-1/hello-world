3a65cc4d4ad394dd9c957bb726726450040730fc
MinusOneTwelfth-1 <56498515+MinusOneTwelfth-1@users.noreply.github.com>
2026-07-30 18:14:33 +0530

    Update lab.js

--- docs/STEMSync/stemsync-github-audio/lab.js
+++ docs/STEMSync/stemsync-github-audio/lab.js
@@ -215,8 +215,9 @@ console.log('lab.js')
   
         ctx.clearRect(0, 0, canvas.width, canvas.height);
          
-       if(upBoing ){ctx.drawImage(uparro,anchorX,y+20); upBoing=false; holdBoing=true; boingCount=35 } // flash the up arrow during pump boing
-       if(holdBoing){boingCount=boingCount-1; ctx.drawImage(uparro,anchorX,y+20); 
+       if(upBoing ){ctx.drawImage(uparro,anchorX-20,y+20); upBoing=false; holdBoing=true; boingCount=35 } // flash the up arrow during pump boing
+       if(holdBoing){boingCount=boingCount-1; ctx.drawImage(uparro,anchorX-20,y+25
+                                                           ); 
                       if (boingCount<=0) { upBoing=false; holdBoing=false;
 
                                        }
