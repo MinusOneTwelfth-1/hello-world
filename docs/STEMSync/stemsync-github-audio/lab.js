a14a486aa9835485a37a49b55806c5049cc72520
MinusOneTwelfth-1 <56498515+MinusOneTwelfth-1@users.noreply.github.com>
2026-08-02 19:09:40 +0530

    Refactor lab.js for improved readability and structure

--- docs/STEMSync/stemsync-github-audio/lab.js
+++ docs/STEMSync/stemsync-github-audio/lab.js
@@ -524,6 +524,6 @@ freq=1/2/Math.PI*Math.sqrt(k/mass)
 pumpThreshold=1/freq/2*1.05
 sigThreshold=1/freq/2/0.9
 
-console.log("lab.js ====  2.6")
+console.log("lab.js ====  2.7")
 
 //# sourceURL=praveen.com/lab.js
