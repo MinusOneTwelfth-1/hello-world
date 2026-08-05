ede15ae6563972d7b5f83b7e5004222b8de4c06e
MinusOneTwelfth-1 <56498515+MinusOneTwelfth-1@users.noreply.github.com>
2026-08-05 11:57:35 +0530

    Update lab.js

--- docs/STEMSync/stemsync-github-audio/lab.js
+++ docs/STEMSync/stemsync-github-audio/lab.js
@@ -343,6 +343,18 @@ function playBoing(ofst=0.2) {   //  ====== AUDIO "BOING"
   source.start(boinkCtx.currentTime,ofst);
 }
 
+
+function drawArrow(x0,y0,x1,y1,color="red"){ctx.strokeStyle=color
+ctx.lineWidth=3
+ctx.beginPath()
+ctx.moveTo(x0,y0)
+ctx.lineTo(x1,y1)
+ctx.lineTo(x1-10,y1-10)
+ctx.moveTo(x1,y1)
+ctx.lineTo(x1+10,y1-10)
+ctx.stroke()}
+
+
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  Physics Engine Loop @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 let tankFrequency = 1/ ( 1/ 2 / Math.PI * Math.sqrt (k/mass)  )
 let pumpFrequency= tankFrequency * 2.05
@@ -529,6 +541,6 @@ freq=1/2/Math.PI*Math.sqrt(k/mass)
 pumpThreshold=1/freq/2*1.05/2
 sigThreshold=1/freq/2/0.9
 
-console.log("lab.js ====  3.8")
+console.log("lab.js ====  3.9")
 
 //# sourceURL=praveen.com/lab.js
